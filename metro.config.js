def2e31fb482faea84bf085f7d2120a408ad9558
42potatohead <zaid42amman@gmail.com>
2025-08-28 11:40:30 +0300

    Android Version Working

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,20 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
 // Configure source extensions
 config.resolver.sourceExts = [...config.resolver.sourceExts, 'jsx', 'js', 'ts', 'tsx'];
+
+// Watch iOS module directory for cross-platform development
+config.watchFolders = [
+  path.resolve(__dirname, 'modules/vpn-module/ios'),
+];
+
+// Resolve modules directory for iOS native modules
+config.resolver.alias = {
+  '@modules': path.resolve(__dirname, 'modules'),
+};
 
 module.exports = withNativeWind(config, { input: './app/global.css' });
